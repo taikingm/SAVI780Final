2c360881be43e085b0222af5578704cd9a328327
Matt Roosa <taiking00matthew@gmail.com>
2018-10-17 23:17:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,60 +2,69 @@
 var mapOne = L.map('mapOne').setView([35.709970, 139.733128], 12);
   // Add base layer
   L.tileLayer('https://api.mapbox.com/styles/v1/taiking/cjnaytlxt07cw2so3m61mo0lf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGFpa2luZyIsImEiOiJjamx1MzJ4NXkwZnd1M2twZWEwb281ZmtlIn0.TjccQyOx5TTdyCCZKnCTrQ',{
-    maxZoom: 18,
+    maxZoom: 13,
     minZoom: 11,
   }).addTo(mapOne);
-fetch('/Users/Matthew/Google Drive/SAVI-780/Final Project/HTML Files/allfiles/NewYorkMetro.geojson')
+fetch('https://raw.githubusercontent.com/taikingm/SAVI780Final/master/allfiles/TokyoMetro.geojson')
   .then(function (response){
     return response.json();
   })
   .then(function (data){
-    L.geoJson(data).addTo(mapOne);
+  var lineInfo = L.geoJson(data);
+    lineInfo.bindPopup(function (layer){
+      return layer.feature.properties['Name (eng)']+ '<br> Created In: ' + layer.feature.properties['commencement'] + '<br> Number of Stations: ' + layer.feature.properties['stations'] + '<br> How long is the line? ' + layer.feature.properties['distant'] + 'km';
+    });
+    lineInfo.addTo(mapOne);
   });
 var mapTwo = L.map('mapTwo').setView([48.853259,2.347954], 12);
   // Add base layer
   L.tileLayer('https://api.mapbox.com/styles/v1/taiking/cjnaytlxt07cw2so3m61mo0lf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGFpa2luZyIsImEiOiJjamx1MzJ4NXkwZnd1M2twZWEwb281ZmtlIn0.TjccQyOx5TTdyCCZKnCTrQ',{
-    maxZoom: 18,
+    maxZoom: 13,
     minZoom: 11,
   }).addTo(mapTwo);
-
-var mapThree = L.map('mapThree').setView([31.237840, 121.463265], 12);
-  // Add base layer
-  L.tileLayer('https://api.mapbox.com/styles/v1/taiking/cjnaytlxt07cw2so3m61mo0lf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGFpa2luZyIsImEiOiJjamx1MzJ4NXkwZnd1M2twZWEwb281ZmtlIn0.TjccQyOx5TTdyCCZKnCTrQ',{
-    maxZoom: 18,
-    minZoom: 11,
-  }).addTo(mapThree);
-
-var mapFour = L.map('mapFour').setView([40.725957, -73.975436], 12);
-  // Add base layer
-  L.tileLayer('https://api.mapbox.com/styles/v1/taiking/cjnaytlxt07cw2so3m61mo0lf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGFpa2luZyIsImEiOiJjamx1MzJ4NXkwZnd1M2twZWEwb281ZmtlIn0.TjccQyOx5TTdyCCZKnCTrQ',{
-    maxZoom: 18,
-    minZoom: 11,
-  }).addTo(mapFour);
-  fetch('/Users/Matthew/Google Drive/SAVI-780/Final Project/HTML Files/allfiles/NewYorkMetro.geojson')
+  fetch('https://raw.githubusercontent.com/taikingm/SAVI780Final/master/allfiles/ParisMetro.geojson')
     .then(function (response){
       return response.json();
     })
     .then(function (data){
-      L.geoJson(data).addTo(mapFour);
+    var lineInfo = L.geoJson(data);
+    lineInfo.bindPopup(function (layer){
+      return layer.feature.properties['name'] + '<br> Created In: ' + layer.feature.properties['Commencement'] + '<br> Number of Stations: ' + layer.feature.properties['Stations'] + '<br> How long is the line? ' + layer.feature.properties['Length'] + 'km';
     });
+    lineInfo.addTo(mapTwo);
+  });
+
+var mapThree = L.map('mapThree').setView([31.237840, 121.463265], 12);
+  L.tileLayer('https://api.mapbox.com/styles/v1/taiking/cjnaytlxt07cw2so3m61mo0lf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGFpa2luZyIsImEiOiJjamx1MzJ4NXkwZnd1M2twZWEwb281ZmtlIn0.TjccQyOx5TTdyCCZKnCTrQ',{
+    maxZoom: 13,
+    minZoom: 11,
+  }).addTo(mapThree);
+  fetch('https://raw.githubusercontent.com/taikingm/SAVI780Final/master/allfiles/ShanghaiMetro.geojson')
+    .then(function (response){
+      return response.json();
+    })
+    .then(function (data){
+    var lineInfo = L.geoJson(data);
+    lineInfo.bindPopup(function (layer){
+      return layer.feature.properties['name'] + '<br> Created In: ' + layer.feature.properties['Commencement'] + '<br> Number of Stations: ' + layer.feature.properties['Stations'] + '<br> How long is the line? ' + layer.feature.properties['Length'] + 'km';
+    });
+    lineInfo.addTo(mapThree);
+  });
 
 
-
-
-function myFunction() {
-     document.getElementById("myDropdown").classList.toggle("show");
-  }
-  window.onClick = function(event) {
-      if (!event.target.matches('.menu')) {
-
-          var dropdowns = document.getElementsByClassName("dropdown-content");
-          var i;
-          for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-              openDropdown.classList.remove('show');
-            }
-          }
-      }
-    }
+var mapFour = L.map('mapFour').setView([40.725957, -73.975436], 12);
+  L.tileLayer('https://api.mapbox.com/styles/v1/taiking/cjnaytlxt07cw2so3m61mo0lf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGFpa2luZyIsImEiOiJjamx1MzJ4NXkwZnd1M2twZWEwb281ZmtlIn0.TjccQyOx5TTdyCCZKnCTrQ',{
+    maxZoom: 13,
+    minZoom: 11,
+  }).addTo(mapFour);
+  fetch('https://raw.githubusercontent.com/taikingm/SAVI780Final/master/allfiles/NewYorkMetro.geojson')
+  .then(function (response){
+    return response.json();
+  })
+  .then(function (data){
+    var lineInfo = L.geoJson(data);
+    lineInfo.bindPopup(function (layer){
+      return layer.feature.properties['name'] + ' train<br> Created In: ' + layer.feature.properties['Commencement'] + '<br> Number of Stations: ' + layer.feature.properties['Stations'] + '<br> How long is the line? ' + layer.feature.properties['Length'] + 'km';
+    });
+    lineInfo.addTo(mapFour);
+  });
